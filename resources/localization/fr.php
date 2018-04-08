dcc328e1be9a55706404ef4d275ef47525cf4f78
Damien Regad <dregad@mantisbt.org>
2018-04-08 11:25:21 +0200

    French translation

--- resources/localization/fr.php
+++ resources/localization/fr.php
@@ -6,5 +6,10 @@ return [
     'usage-options' => 'options',
     'usage-operands' => 'arguments',
     'options-title' => "Options:\n",
-    'commands-title' => "Commandes:\n"
+    'commands-title' => "Commandes:\n",
+    'option-unknown' => 'Option \'%s\' inconnue',
+    'no-more-operands' => 'Argument inattendu - %s',
+    'operand-missing' => 'L\'argument %s est obligatoire',
+    'option-argument-missing' => 'L\'option \'%s\' doit avoir une valeur',
+    'option-value-invalid' => 'La valeur de l\'option \'%s\' est invalide',
 ];
