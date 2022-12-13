91c31c9bc0ff9bbe22b0b02c63c7f5ddb8d2a8d5
Thomas Flori <thflori@gmail.com>
2022-12-13 21:37:45 +0100

    add Catalan localization

---
+++ resources/localization/ca.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'usage-title' => 'Ús: ',
+    'usage-command' => 'ordre',
+    'usage-options' => 'opcions',
+    'usage-operands' => 'operands',
+    'operands-title' => "Operands:" . PHP_EOL,
+    'options-title' => "Opcions:" . PHP_EOL,
+    'commands-title' => "Ordres:" . PHP_EOL,
+    'option-unknown' => 'L\'opció \'%s\' és desconeguda',
+    'no-more-operands' => 'No s\'esperen més operands - obtingut %s',
+    'operand-missing' => 'L\'operand %s és necessari',
+    'option-argument-missing' => 'L\'opció \'%s\' ha de tenir un valor',
+    'value-invalid' => '%s té un valor invàlid',
+];
